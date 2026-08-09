5fd0f0d3e7bca61653ea3e35a71f5d044a2187a0
Tamanna <tamanna.five@gmail.com>
2026-08-09 16:14:48 +0530

    Time: 7 ms (31.75%), Space: 44.5 MB (66.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int count[] = new int[26];
-        char ch[] = s.toCharArray();
-        char che[] = t.toCharArray();
-        for(char c: ch){
-            count[c - 'a']++;
-        }
-
-        for(char c: che){
-            count[c - 'a']--;
-        }
-        for(int i : count){
-        if(i != 0){
+        if(s.length() != t.length()){
             return false;
         }
-    }
+
+        int count[] = new int[26];
+        for(int i = 0; i<s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int i:count){
+            if(i!=0){
+                return false;
+            }
+        }
         return true;
+        
+        }
     }
-}
