314f4706ba0b66b3aa1cbc08db527e9237ed293d
Tamanna <tamanna.five@gmail.com>
2026-08-09 16:10:33 +0530

    Time: 0 ms (100%), Space: 43.5 MB (17.01%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String str = strs[0];
+        for(int i = 1; i<strs.length; i++){
+            while(!strs[i].startsWith(str)){
+                str = str.substring(0,str.length()-1);
+            }
+        }
+        if(str.isEmpty()){
+            return "";
+        }
+        return str;
+    }
+}
