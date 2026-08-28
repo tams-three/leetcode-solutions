0bee5e0c3d54f4805b27d7ace27e8fba36bb0e9b
Tamanna <tamanna.five@gmail.com>
2026-08-28 21:33:21 +0530

    Time: 18 ms (12.91%), Space: 73.6 MB (33.05%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,17 +1,17 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int missingno= 1;
         Arrays.sort(nums);
-        for(int num:nums){
-            if(num>0){
-                if(num == missingno){
-                    missingno++;
-                }
-                else if(num>missingno){
-                    break;
-                }
+       int missingno = 1;
+       for(int i : nums){
+        if(i>0){
+            if(i==missingno){
+                missingno++;
+            }
+            if(i>missingno){
+                break;
             }
         }
-        return missingno;
+       }
+       return missingno;
     }
 }
