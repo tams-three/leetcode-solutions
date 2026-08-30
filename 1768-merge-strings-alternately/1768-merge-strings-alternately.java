ba10444e0dc87edf1c02fc7f5a5f1bd434a7e4e0
Tamanna <tamanna.five@gmail.com>
2026-08-30 16:39:01 +0530

    Time: 1 ms (96.08%), Space: 43.4 MB (24.55%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -2,7 +2,7 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         int i = 0;
         int j = 0;
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         while(i<word1.length() || j<word2.length()){
             if(i<word1.length()){
                 sb.append(word1.charAt(i));
@@ -12,6 +12,7 @@ class Solution {
                 sb.append(word2.charAt(j));
                 j++;
             }
+        
         }
         return sb.toString();
     }
